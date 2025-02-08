31d9118660f3bdd1d3615e301c8a11f3591f8a30
prime7000 <agbadutaofejiro123@gmail.com>
2025-02-08 02:03:16 +0100

    prime tempory interface

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('',views.home, name = 'home')
+    path('',views.home, name = 'home'),
+    path('image_upload', views.image_upload, name = 'image_upload'),
+    path('records/', views.records, name = 'record')
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render,redirect
 def home(request):
 
     return render(request, 'home.html')
+
+def image_upload(request):
+
+    return render(request, 'image_upload.html')
+
+def records(request):
+
+    return render(request, 'record.html')
